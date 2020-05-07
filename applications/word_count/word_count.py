b2c3587f61a69762bd16a19c9b0a7914188cfb65
thesoundfromthesky <3rdworldstrawberry@gmail.com>
2020-05-07 17:40:53 -0500

    Day 4 word_count.py

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,5 +1,19 @@
+import re
+rxp = r'[":;,.\-+=/\\|\[\]{}()*\^&]'
 def word_count(s):
     # Implement me.
+    s = s.casefold()
+    s = re.sub(rxp,"", s)
+    word_list = s.split()
+    word_dict = {}
+    while len(word_list):
+        word = word_list.pop()
+        if word not in word_dict:
+            word_dict[word] = 1
+        else:
+            word_dict[word] += 1
+    return word_dict
+
 
 
 if __name__ == "__main__":
