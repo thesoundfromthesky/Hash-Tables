1c341bf22bee0bef09ff47aa2a716ce55912cae5
thesoundfromthesky <3rdworldstrawberry@gmail.com>
2020-05-07 22:40:59 -0500

    Day 4 markov

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -1,8 +1,32 @@
 import random
+import re
 
 # Read in all the words in one go
-with open("input.txt") as f:
+# with open("input.txt") as f:
+with open("./applications/markov/test.txt") as f:
     words = f.read()
+    word_dict={}
+    word_list = words.split()
+    word_start={}
+    word_end={}
+    for w in word_list:
+        if w not in word_dict:
+            escape_w=re.escape(w)
+            rxp_start= rf"(?<={escape_w}\s)[\w\-,\"';:!?.()]+(?=\s?)"
+            rxp = rf"(?<={escape_w}\s)[\w\-,\"';:!?.()]+(?=\s?)"
+            word_dict[w] = re.findall(rxp, words)
+    
+    word_keys = list(word_dict.keys())
+    nxt = random.choice(word_keys)
+    st=""
+    while nxt:
+         if not word_dict[nxt]:
+             break
+         nxt = random.choice(word_dict[nxt])
+         st = " ".join([st, nxt])
+
+    print(st)
+
 
 # TODO: analyze which words can follow other words
 
